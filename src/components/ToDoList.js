d15cf3a1ee3b691be1ca848794a53d77ba340c34
marut97 <marut1997@gmail.com>
2021-02-16 08:58:58 +0530

    Added background and file structure

---
+++ src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../styles/ToDoList.css";
+
+export class ToDoList extends Component {
+  render() {
+    return (
+      <div className="ToDoList">
+        <div>To Do List...</div>
+      </div>
+    );
+  }
+}
