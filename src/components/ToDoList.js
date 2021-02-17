2d034ced244cdad2342b0ec13be2965d0c70cda5
marut97 <marut1997@gmail.com>
2021-02-17 21:32:32 +0530

    To do list main page created

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "../styles/ToDoList.css";
+import logo from "../icons/to-do-list.png";
 
 export class ToDoList extends Component {
   render() {
     return (
       <div className="ToDoList">
-        <div>To Do List...</div>
+        <div className="ToDoListHeader">
+          <img className="ToDoListLogo" src={logo} />
+          <label className="ToDoListHeaderLabel">To Do List ...</label>
+        </div>
+        <div className="ToDoListBody"></div>
       </div>
     );
   }
